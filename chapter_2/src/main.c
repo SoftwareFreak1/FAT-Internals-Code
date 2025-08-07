e208f4bcc9c36196f97bb3be451681776fc8abed
Björn Götz <bjoern.goetz2001@gmx.de>
2025-08-07 17:25:41 +0200

    simplify code

--- chapter_2/src/main.c
+++ chapter_2/src/main.c
@@ -4,22 +4,31 @@
 #define VOLUME_LABEL_ADDRESS 0x2B
 #define VOLUME_LABEL_SIZE 11
 
+void read_volume_label(Disk *disk);
+
 int main(int argc, char* argv[]) {
     if(argc != 2) {
         printf("./run.sh [disk file]\n");
         return -1;
     }
 
-    uint8_t buffer[VOLUME_LABEL_SIZE];
+    
     Disk *disk = disk_open(argv[1]);
-
     if(disk == NULL) {
         printf("can't open disk file\n");
         return -1;
     }
 
-    disk_read(disk, VOLUME_LABEL_ADDRESS, VOLUME_LABEL_SIZE, buffer);
+    read_volume_label(disk);
+
     disk_close(disk);
+
+    return 0;
+}
+
+void read_volume_label(Disk *disk) {
+    uint8_t buffer[VOLUME_LABEL_SIZE];
+    disk_read(disk, VOLUME_LABEL_ADDRESS, VOLUME_LABEL_SIZE, buffer);
 
     // null terminates volume label
     char volume_label[VOLUME_LABEL_SIZE + 1];
@@ -27,6 +36,4 @@ int main(int argc, char* argv[]) {
     volume_label[VOLUME_LABEL_SIZE] = '\0';
 
     printf("The volume label is '%s'\n", volume_label);
-
-    return 0;
 }
